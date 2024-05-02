5b8d1d24b8d8f612fdf06e2ae1079d8a15ed4076
SINTU KUMAR <topcurrentaffairs09@gmail.com>
2024-05-02 22:08:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import { TextField, Typography } from '@mui/material';
+import { Box, Typography, TextField } from '@mui/material';
 import { findAll } from "highlight-words-core";
 import './App.css'; // Import CSS for styling
 
 const App = () => {
   // Initial text and search words for highlighting
-  const initialTextToHighlight = "This is some text to highlight.";
+  const initialTextToHighlight = "This is some text to highlight.Commodo occaecat officia cillum sunt ipsum commodo proident dolore. Sit esse eiusmod nulla occaecat pariatur ea ex aliquip proident fugiat sunt voluptate. Pariatur qui tempor excepteur officia esse sunt consectetur enim exercitation enim fugiat. Officia reprehenderit voluptate culpa adipisicing reprehenderit cillum elit aute tempor anim. Nulla ullamco do aliqua consequat ad ut amet excepteur adipisicing duis consequat. Eu commodo nostrud nostrud aliqua incididunt ea excepteur pariatur ea in cillum in. Ea culpa et quis dolore labore pariatur sit non officia ex esse ullamco. Minim tempor Lorem excepteur cillum aliqua. Officia sit consectetur anim eu minim adipisicing eu amet magna et. Ipsum id amet voluptate ullamco ad excepteur irure pariatur nulla do Lorem. In nulla veniam ipsum occaecat veniam ea nisi irure nisi commodo duis consectetur irure ea.";
   const initialSearchWords = ["This", "i"];
 
   // State for text to highlight and search words
@@ -36,9 +36,9 @@ const App = () => {
   });
 
   return (
-    <div className="app-container">
+    <Box className="app-container" textAlign="left">
       <Typography variant="h4" gutterBottom>Text Highlighter</Typography>
-      <div className="search-container">
+      <Box className="search-container">
         <TextField
           label="Search text..."
           variant="outlined"
@@ -46,11 +46,11 @@ const App = () => {
           value={searchWords.join(' ')}
           onChange={handleSearchInputChange}
         />
-      </div>
+      </Box>
       <div className="highlighted-text" id="app">
         {highlightedText}
       </div>
-    </div>
+    </Box>
   );
 }
 
