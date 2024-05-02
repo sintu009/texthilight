6e97302a00d55d98bc655f9b0e5bc83db44243f7
SINTU KUMAR <topcurrentaffairs09@gmail.com>
2024-05-02 22:03:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
+import { TextField, Typography } from '@mui/material';
 import { findAll } from "highlight-words-core";
 import './App.css'; // Import CSS for styling
 
 const App = () => {
   // Initial text and search words for highlighting
-  const initialTextToHighlight = "This is some text to highlight. Pariatur enim tempor anim non qui excepteur in irure culpa. Esse enim reprehenderit incididunt do irure ex duis. Cupidatat quis velit est tempor occaecat minim in quis. Elit consequat minim velit anim eiusmod. Eu cillum pariatur ea reprehenderit nostrud adipisicing enim. Elit proident sunt id non veniam dolore minim esse irure eiusmod. Cupidatat ipsum laboris occaecat proident anim eu proident do. Id quis tempor eiusmod consequat aliqua enim nostrud ex sit fugiat occaecat ullamco adipisicing. Adipisicing eu magna cillum nulla veniam velit adipisicing culpa quis culpa velit et do. Nostrud mollit eiusmod enim officia laborum ipsum aute quis esse reprehenderit ea ipsum eu. Voluptate amet elit pariatur officia esse est quis. Adipisicing cillum laborum commodo tempor quis tempor veniam nisi enim ex nostrud. Ex ipsum cupidatat aute reprehenderit consectetur duis. Do ipsum commodo anim id ea aliquip in minim et. Anim consectetur magna proident pariatur ex ut est laborum non aute.";
+  const initialTextToHighlight = "This is some text to highlight.";
   const initialSearchWords = ["This", "i"];
 
   // State for text to highlight and search words
@@ -13,7 +14,7 @@ const App = () => {
 
   // Function to handle search input change
   const handleSearchInputChange = (event) => {
-    setSearchWords([event.target.value]);
+    setSearchWords(event.target.value.split(' '));
   };
 
   // Find all occurrences of search words in the text
@@ -36,13 +37,13 @@ const App = () => {
 
   return (
     <div className="app-container">
-      <h1>Text Highlighter</h1>
+      <Typography variant="h4" gutterBottom>Text Highlighter</Typography>
       <div className="search-container">
-        <input
-          type="text"
-          className="search-input"
-          placeholder="Search text..."
-          value={searchWords[0]}
+        <TextField
+          label="Search text..."
+          variant="outlined"
+          fullWidth
+          value={searchWords.join(' ')}
           onChange={handleSearchInputChange}
         />
       </div>
